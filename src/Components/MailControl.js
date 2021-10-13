827663a7256b39dc055513e3c14942993732abe6
oaknb <oaknbursa@gmail.com>
2021-10-13 15:00:06 +0300

    Mail kontrol icon eklendi

--- src/Components/MailControl.js
+++ src/Components/MailControl.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Button } from 'antd';
-
+import { Button,Avatar } from 'antd';
+import {CheckOutlined  } from'@ant-design/icons';
 
 
 function MailControl() {
@@ -11,6 +11,10 @@ function MailControl() {
         <div className="featuredItem" style={{marginTop:'50px'}}>
       
         <div className='divCenter' style={{textAlign:'center'}}>
+        < Avatar style={{backgroundColor:'green'}} size={{ xs: 24, sm: 32, md: 40, lg: 64, xl: 80, xxl: 100 }}
+    icon={<CheckOutlined   />}/>
+    <hr className="home-top__seperator" style={{width:'300px', marginTop:'30px', marginBottom:'30px'}} />
+
            <h1>WELCOME</h1> 
            
            <h1>Your e-mail has been confirmed</h1>
--- src/Components/MailError.js
+++ src/Components/MailError.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { Button } from 'antd';
-
+import { Button,Avatar } from 'antd';
+import {CloseOutlined   } from'@ant-design/icons';
 
 function MailError() {
     return (
@@ -8,10 +8,14 @@ function MailError() {
         <div className="featuredItem" style={{marginTop:'50px'}}>
       
         <div className='divCenter' style={{textAlign:'center'}}>
+        
+       < Avatar style={{backgroundColor:'red'}} size={{ xs: 24, sm: 32, md: 40, lg: 64, xl: 80, xxl: 100 }}
+    icon={<CloseOutlined    />}/>
+    <hr className="home-top__seperator" style={{width:'300px', marginTop:'30px', marginBottom:'30px'}} />
            <h1>ERROR</h1> 
            
            <h1>An error was encountered while confirming the mail. please contact admin</h1>
-           
+
            <hr className="home-top__seperator" style={{width:'300px', marginTop:'30px', marginBottom:'30px'}} />
            <Button  type="primary" a href="http://localhost:3000/">Continue to web site</Button>
            
